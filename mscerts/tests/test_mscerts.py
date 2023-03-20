a4b16a2497836ef29cfe0627d6fe654916c49dd4
Ralph Broenink <ralph@ralphbroenink.net>
2023-03-20 14:17:44 +0100

    Fix and test STL paths

--- mscerts/tests/test_mscerts.py
+++ mscerts/tests/test_mscerts.py
@@ -7,6 +7,14 @@ def test_cabundle_exists() -> None:
     assert os.path.exists(mscerts.where())
 
 
+def test_stl_exists() -> None:
+    assert os.path.exists(mscerts.where(stl=True))
+
+
+def test_cabundle_is_different_from_stl() -> None:
+    assert mscerts.where() != mscerts.where(stl=True)
+
+
 def test_read_contents() -> None:
     content = mscerts.contents()
     assert "-----BEGIN CERTIFICATE-----" in content
