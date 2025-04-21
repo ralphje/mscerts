a7a5e7292a75c4c203efce923b42f3b89012152b
Ralph Broenink <ralph@ralphbroenink.net>
2025-04-21 13:08:08 +0200

    Update to latest base repository

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     },
     zip_safe=False,
     license="MPL-2.0",
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -51,7 +51,6 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
